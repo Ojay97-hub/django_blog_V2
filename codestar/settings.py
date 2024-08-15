d446d533af7bdd36051fb546a66cf5d4db4ba457
Owen Cotter <owenjames97@outlook.com>
2024-08-15 22:39:59 +0000

    add csrf

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=s!^+_td$boh%v*p)dkhhc%)6osjqvo6m)-l3np&j^0durs3-'
+# SECRET_KEY = 'django-insecure-3=s!^+_td$boh%v*p)dkhhc%)6osjqvo6m)-l3np&j^0durs3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -90,6 +90,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
